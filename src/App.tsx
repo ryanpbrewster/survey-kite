3f91f3659b1220b0139c6e18f80d8eecd9f23fb2
Ryan P. Brewster <RyanPBrewster@gmail.com>
2020-07-11 22:57:30 -0700

    add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,38 @@
 import React from "react";
-import logo from "./logo.svg";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useParams,
+  Redirect,
+} from "react-router-dom";
 import "./App.css";
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <img src={logo} className="App-logo" alt="logo" />
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/take/:surveyId">
+          <TakeSurveyPage />
+        </Route>
+        <Route path="/edit">
+          <EditPage />
+        </Route>
+        <Route>
+          <Redirect to="/edit" />
+        </Route>
+      </Switch>
+    </Router>
   );
+};
+
+const EditPage: React.FC = () => {
+  return <p>Time to edit a survey!</p>;
+};
+
+const TakeSurveyPage: React.FC = () => {
+  const { surveyId } = useParams();
+  return <p>Survey {surveyId}</p>;
 };
 
 export default App;
